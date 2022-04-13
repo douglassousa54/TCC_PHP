2080d69bc42e6a258b767d045bd564232e84ba15
unknown <douglas.sousa54@etec.sp.gov.br>
2022-04-12 22:27:05 -0300

    incluse.php

---
+++ Desktop/TCC/sobre.php
@@ -0,0 +1,62 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Descomplica Vet</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="body.css">
+    <style type="text/css">
+    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</head>
+<body>
+
+<Main class= "main">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Descomplica Vet</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="https://www.simples.vet/">Sobre</a> 
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="https://www.simples.vet/precos/">Valor</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Criadores</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Teste Gratis</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Contatos</a>
+        </li>
+      </ul>
+        </div>
+  </div>
+</nav>
+</Main>
+<div>
+    <p> Dedicação e vontade de contribuir para o crescimento do se comercio Pet.
+Pensamos nesta solução para as empresas na época do ETEC, em 2022. Desde então, buscamos melhorar nossos conhecimentos e com isso deixar a ferramenta cada vez mais intuitiva e funcional para os nossos clientes e clientes dos nossos clientes.
+Hoje estamos 100% dedicados a atender as necessidades dos nossos clientes, afim de melhorar o aplicativo e desta forma trazer mais usuários para a plataforma. Desta forma garantindo a satisfação de todos!
+</p>
+ 
+<h2>Missão</h2>
+<p>Ser simples no atendimento e com disse atender as necessidades de nossas lojas pets e seus clientes!</p>
+<h2>Visão</h2>
+<p>Simplificar para facilitar o relacionamento loja/cliente </p>
+<h2>Valores</h2>
+<p>Simplicidade é a base.
+Cliente em primeiro lugar 
+</p>
+</div>
